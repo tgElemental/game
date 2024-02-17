d9e8dd48ad6d4098443df108886f67fe51730f79
Javid <javid.momeni@gmail.com>
2024-02-17 16:57:42 +0330

    build

--- src/pages/ElementsPage.tsx
+++ src/pages/ElementsPage.tsx
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 import Nav from "../components/Nav";
 import WebApp from "@twa-dev/sdk";
+import { Card, DotLoading, Grid } from "antd-mobile";
+import data from "@emoji-mart/data";
+import { init } from "emoji-mart";
+init({ data });
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      "em-emoji": any;
+    }
+  }
+}
 interface ScoreData {
   "0": {
     userid: number;
@@ -10,15 +23,18 @@ interface ScoreData {
     water: number;
     wind: number;
     fire: number;
+    earth: number;
   };
 }
 const ElementsPage = () => {
   const [scoreData, setScoreData] = useState<ScoreData | null>(null);
+  const [isLoading, setIsLoading] = useState(true); // New state for loading status
   const userdata = WebApp.initDataUnsafe;
-  const userid = userdata?.user?.id;
+  const userid = userdata?.user?.id || 208627;
 
   useEffect(() => {
     const fetchScore = async () => {
+      setIsLoading(true); // Set loading to true before fetching
       try {
         const response = await fetch(
           `https://api.rahomaskan.com/api/score?tgid=${userid}`,
@@ -27,6 +43,8 @@ const ElementsPage = () => {
         setScoreData(data);
       } catch (error) {
         console.error("Error fetching score data:", error);
+      } finally {
+        setIsLoading(false); // Set loading to false after fetching
       }
     };
 
@@ -37,18 +55,87 @@ const ElementsPage = () => {
     <>
       <Nav />
       <h1>صفحه عناصر</h1>
-      {scoreData && (
+      {isLoading ? (
+        <DotLoading /> // Show the loading indicator when data is being fetched
+      ) : scoreData ? (
         <div>
           <h2>User Score</h2>
+
+          <Grid columns={2} gap={8}>
+            <Grid.Item span={2}>
+              <Card
+                bodyStyle={{ background: "#eee" }}
+                headerStyle={{ background: "#ddd" }}
+                extra={<em-emoji id="moneybag"></em-emoji>}
+                title="امتیاز"
+              >
+                {scoreData["0"].score.toString()}
+              </Card>
+            </Grid.Item>
+            <Grid.Item>
+              <Card
+                bodyStyle={{ background: "#ccc" }}
+                headerStyle={{ background: "#ddd" }}
+                extra={<em-emoji id="deciduous_tree"></em-emoji>}
+                title="درخت"
+              >
+                {scoreData["0"].tree.toString()}
+              </Card>
+            </Grid.Item>
+            <Grid.Item>
+              <Card
+                bodyStyle={{ background: "#ccc" }}
+                headerStyle={{ background: "#ddd" }}
+                extra={<em-emoji id="sunny"></em-emoji>}
+                title="نور"
+              >
+                {scoreData["0"].light.toString()}
+              </Card>
+            </Grid.Item>
+            <Grid.Item>
+              <Card
+                bodyStyle={{ background: "#ccc" }}
+                headerStyle={{ background: "#ddd" }}
+                extra={<em-emoji id="droplet"></em-emoji>}
+                title="آب"
+              >
+                {scoreData["0"].water.toString()}
+              </Card>
+            </Grid.Item>
+            <Grid.Item>
+              <Card
+                bodyStyle={{ background: "#ccc" }}
+                headerStyle={{ background: "#ddd" }}
+                extra={<em-emoji id="wind_blowing_face"></em-emoji>}
+                title="باد"
+              >
+                {scoreData["0"].wind.toString()}
+              </Card>
+            </Grid.Item>
+            <Grid.Item>
+              <Card
+                bodyStyle={{ background: "#ccc" }}
+                headerStyle={{ background: "#ddd" }}
+                extra={<em-emoji id="fire"></em-emoji>}
+                title="آتش"
+              >
+                {scoreData["0"].fire.toString()}
+              </Card>
+            </Grid.Item>
+            <Grid.Item>
+              <Card
+                bodyStyle={{ background: "#ccc" }}
+                headerStyle={{ background: "#ddd" }}
+                extra={<em-emoji id="large_brown_circle"></em-emoji>}
+                title="خاک"
+              >
+                {scoreData["0"].earth.toString()}
+              </Card>
+            </Grid.Item>
+          </Grid>
           <p>User ID: {scoreData["0"].userid}</p>
-          <p>Score: {scoreData["0"].score}</p>
-          <p>Tree: {scoreData["0"].tree}</p>
-          <p>Light: {scoreData["0"].light}</p>
-          <p>Water: {scoreData["0"].water}</p>
-          <p>Wind: {scoreData["0"].wind}</p>
-          <p>Fire: {scoreData["0"].fire}</p>
         </div>
-      )}
+      ) : null}
     </>
   );
 };
