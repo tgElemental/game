3d88b3934c6dba0cf868b48dff69f83859fa8608
Javid <javid.momeni@gmail.com>
2024-02-17 15:24:55 +0330

    build

--- src/pages/RankingPage.tsx
+++ src/pages/RankingPage.tsx
@@ -1,9 +1,45 @@
+import { useState, useEffect } from "react";
 import Nav from "../components/Nav";
+import { List, DotLoading } from "antd-mobile";
+
+interface RankingItem {
+  name: string;
+  score: number;
+}
+
 const RankingPage = () => {
+  const [data, setData] = useState<RankingItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.rahomaskan.com/api/ranking");
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Nav />
-      <h1>صفحه رده بندی</h1>
+      {loading ? (
+        <DotLoading />
+      ) : (
+        <List mode="card" header="رده بندی">
+          {data.map((item, index) => (
+            <List.Item title={index + 1} extra={item.score} key={index}>
+              {item.name}
+            </List.Item>
+          ))}
+        </List>
+      )}
     </>
   );
 };
