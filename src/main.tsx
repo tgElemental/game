ea05ea0b99c80b1f642dea882fc1d4e7fc81e821
Javid <javid.momeni@gmail.com>
2024-02-18 11:03:15 +0330

    build

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import "./index.css";
 import { clarity } from "react-microsoft-clarity";
 
 clarity.init("kzhxxy2ip7");
+clarity.consent();
+clarity.setTag("miniapp", "game");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
